5c5d069625cae01a2deda2680d577c5eb0f15c79
Marko <neonmarko2@gmail.com>
2026-03-25 14:13:58 +0100

    Initial commit

---
+++ src/main/java/net/neonmarko2/skeletonmessenger/SkeletonMessengerClient.java
@@ -0,0 +1,10 @@
+package net.neonmarko2.skeletonmessenger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkeletonMessengerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/neonmarko2/skeletonmessenger/SkeletonMessengerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.neonmarko2.skeletonmessenger;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkeletonMessengerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
