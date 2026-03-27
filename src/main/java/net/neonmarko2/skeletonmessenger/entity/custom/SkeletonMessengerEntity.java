1be2297f06b08e57ea9edd186b6d015c8409b127
Marko <neonmarko2@gmail.com>
2026-03-27 19:29:30 +0100

    Change how Messenger behaves The messenger no longer teleports from one player to another. Now a new Messenger is spawned and the old one is removed. The messenger disappears if the caller leaves the game without giving it an item. Writing and Reading custom data is now done purely on the server.

--- src/main/java/net/neonmarko2/skeletonmessenger/entity/custom/SkeletonMessengerEntity.java
+++ src/main/java/net/neonmarko2/skeletonmessenger/entity/custom/SkeletonMessengerEntity.java
@@ -12,12 +12,15 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.neonmarko2.skeletonmessenger.entity.ModEntities;
 
 import java.util.Objects;
 
@@ -54,36 +57,46 @@ public class SkeletonMessengerEntity extends MobEntity{
                 discard();
                 return;
             }
+            if(itemToMail.isEmpty() && caller == null){
+                discard();
+                return;
+            }
+            setVelocity(0, 0, 0);
         }
         super.tick();
     }
 
     @Override
     public void writeCustomDataToNbt(NbtCompound nbt) {
-        if (!itemToMail.isEmpty()){
-            nbt.put("MailItem", itemToMail.encode(getRegistryManager()));
+        if(!getWorld().isClient){
+            if (!itemToMail.isEmpty()){
+                nbt.put("MailItem", itemToMail.encode(getRegistryManager()));
+            }
+            if(owner != null){
+                nbt.putUuid("Owner", owner.getUuid());
+            }
+            if(caller != null){
+                nbt.putUuid("Caller", caller.getUuid());
+            }
+            super.writeCustomDataToNbt(nbt);
         }
-        if(owner != null){
-            nbt.putUuid("Owner", owner.getUuid());
-        }
-        if(caller != null){
-            nbt.putUuid("Caller", caller.getUuid()); /// CALLER AND OR OWNER NOT WORKING PROPERLY
-        }
-        super.writeCustomDataToNbt(nbt);
     }
 
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
-        if(Objects.requireNonNull(getServer()).getPlayerManager().getPlayer(nbt.getUuid("Owner")) != null){
-            this.owner = getServer().getPlayerManager().getPlayer(nbt.getUuid("Owner"));
-            System.out.println("Owner is on the server");
+        if(!getWorld().isClient){
+            System.out.println(getServer().getPlayerManager().getPlayer(nbt.getUuid("Owner")));
+            if(Objects.requireNonNull(getServer()).getPlayerManager().getPlayer(nbt.getUuid("Owner")) != null){
+                this.owner = getServer().getPlayerManager().getPlayer(nbt.getUuid("Owner"));
+                System.out.println("Owner is on the server");
+            }
+            if(getServer().getPlayerManager().getPlayer(nbt.getUuid("Caller")) != null){
+                this.caller = getServer().getPlayerManager().getPlayer(nbt.getUuid("Caller"));
+                System.out.println("Caller is on the server");
+            }
+            itemToMail = ItemStack.fromNbt(getRegistryManager(), nbt.get("MailItem")).orElse(ItemStack.EMPTY);
+            super.readCustomDataFromNbt(nbt);
         }
-        if(getServer().getPlayerManager().getPlayer(nbt.getUuid("Caller")) != null){
-            this.caller = getServer().getPlayerManager().getPlayer(nbt.getUuid("Caller"));
-            System.out.println("Caller is on the server");
-        }
-        itemToMail = ItemStack.fromNbt(getRegistryManager(), nbt.get("MailItem")).orElse(ItemStack.EMPTY);
-        super.readCustomDataFromNbt(nbt);
     }
 
     @Override
@@ -126,14 +139,23 @@ public class SkeletonMessengerEntity extends MobEntity{
                     player.damage(getWorld().getDamageSources().magic(), 1);
                     return ActionResult.FAIL;
                 }
-                itemToMail = player.getStackInHand(hand).copy();
+
+                SkeletonMessengerEntity skeletonMessenger = new SkeletonMessengerEntity(ModEntities.SKELETON_MESSENGER, owner.getWorld());
+                var playerFacing = owner.getEyePos().add(owner.getRotationVec(1.0f).multiply(2));
+                var skeletonSpawnPosition = new Vec3d(playerFacing.x, owner.getY(), playerFacing.z);
+                skeletonMessenger.setPosition(skeletonSpawnPosition);
+                skeletonMessenger.FaceEntityInstantly(owner);
+                skeletonMessenger.caller = caller;
+                skeletonMessenger.owner = owner;
+                skeletonMessenger.itemToMail = player.getStackInHand(hand).copy();
+                owner.getWorld().spawnEntity(skeletonMessenger);
+
+                owner.getWorld().playSound(null, owner.getBlockPos(), SoundEvents.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, SoundCategory.PLAYERS, 1, 1);
                 player.setStackInHand(hand, ItemStack.EMPTY);
                 player.sendMessage(Text.literal("Gave the messenger " + currentPlayerStack.getName().getString()));
-                SpawnInfrontOf(owner);
-                FaceEntityInstantly(owner);
-                timerUntilVanish = vanishTime;
                 caller.removeStatusEffect(StatusEffects.BLINDNESS);
                 owner.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20*1, 1));
+                discard();
             } else {
                 if(player.getUuid().compareTo(owner.getUuid()) != 0){
                     player.damage(getWorld().getDamageSources().magic(), 1);
